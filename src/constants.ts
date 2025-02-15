8ff982e7e498a173dcf38a0a4647726070ca7bfb
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2025-02-14 23:11:41 -0800

    Fix URI_PATTERN regex to escape backslashes for correct matching
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- src/constants.ts
+++ src/constants.ts
@@ -7,7 +7,7 @@ export const INPUT_SCHEME = "input";
 export const SWING_FILE = "codeswing.json";
 export const UNTITLED_SCHEME = "untitled";
 
-export const URI_PATTERN = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+export const URI_PATTERN = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/;
 
 export const ZERO_WIDTH_SPACE = "‎‎\u200B";
 
